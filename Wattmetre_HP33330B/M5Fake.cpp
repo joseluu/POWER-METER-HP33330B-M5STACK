6fa305c815cfca98bef7ddc24f35d318a6b0aef0
joseluu <jose.luu@gmail.com>
2023-03-11 22:58:21 +0100

    add button fonctionality

--- Wattmetre_HP33330B/M5Fake.cpp
+++ Wattmetre_HP33330B/M5Fake.cpp
@@ -4,3 +4,10 @@ M5Fake M5;
 void M5Fake::begin() {
     lcd.begin();
 }
+
+void M5Fake::update() {
+    // update buttons
+    BtnA.read();
+    BtnB.read();
+    BtnC.read();
+}
--- Wattmetre_HP33330B/M5Fake.h
+++ Wattmetre_HP33330B/M5Fake.h
@@ -2,26 +2,22 @@
 #ifndef M5FAKE_H
 #define M5FAKE_H
 #include "M5FakeDevkitOled.h"
+#include "Button.h"
 
-class ButtonFake {
-public:
-    bool wasPressed() {
-        return false;
-    }
-    bool isPressed() {
-        return false;
-    }
-};
+#define BUTTON_A_PIN 0
+#define BUTTON_B_PIN 34
+#define BUTTON_C_PIN 12
 
 class M5Fake {
 public:
     M5Fake(){};
     void begin();
-    void update() {};
+    void update();
     M5FakeDevkitOled Lcd;
-    ButtonFake BtnA;
-    ButtonFake BtnB;
-    ButtonFake BtnC;
+    #define DEBOUNCE_TIME_MS 10
+    Button BtnA = Button(BUTTON_A_PIN, true, DEBOUNCE_TIME_MS);
+    Button BtnB = Button(BUTTON_B_PIN, true, DEBOUNCE_TIME_MS);
+    Button BtnC = Button(BUTTON_C_PIN, true, DEBOUNCE_TIME_MS);
 };
 
 #define lcd Lcd
